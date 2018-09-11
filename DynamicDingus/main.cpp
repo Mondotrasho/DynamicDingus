2472a1a9689221c96f68a7cd9388c5c701962dce
Mondotrasho <oscar@peoplepictures.com.au>
2018-09-11 16:00:35 +1000

    Add project files.

---
+++ DynamicDingus/main.cpp
@@ -0,0 +1,15 @@
+
+#include <stdlib.h>
+#include "DynamicDingus.h"
+
+int main()
+{
+	
+DynamicDingus<int> nums;
+	nums[1] = 1;
+	nums[2] = 1;
+	nums[15] = 1;
+
+	system("pause");
+	return 0;
+}
---
+++ enc_temp_folder/2056452bc8908a1754082c0a43385ab/DynamicDingus.h
@@ -0,0 +1,190 @@
+#pragma once
+template <class TYPE>
+class DynamicDingus
+{
+public:
+
+	//  a.A constructor, destructor, assignment operator and copy constructor
+	DynamicDingus();
+	~DynamicDingus();
+	DynamicDingus(const DynamicDingus<TYPE>*& other);
+	DynamicDingus<TYPE>& operator = (const DynamicDingus<TYPE>& other);
+
+	TYPE& operator[](int index);
+	int getAllocatedInt() const { return allocatedElements / sizeof(TYPE); }
+	int getUsedInt() const { return usedElements / sizeof(TYPE); }
+	void setUsedInt(int a){usedElements = a * sizeof(TYPE)}
+	//	b.Functions for adding and removing from the end of the array
+	void pushback(TYPE value);
+	void pushfront(TYPE value);
+	bool isEmpty() const { return usedElements == 0; }
+	void Clear();
+
+	void pushbefore(TYPE value);
+	void pushafter(TYPE value);
+		TYPE pop();
+
+		void set(size_t index, TYPE value);
+		TYPE get(size_t index);
+
+		size_t size();
+		size_t capacity();
+		void print();
+
+	private:
+		size_t allocatedElements;
+		size_t usedElements;
+		//int allocated;
+		//int used;
+		TYPE* theArray;
+		//size_t initialSize ;
+		int spaceMulti = 10;//number of spots
+		//int nextIndex; // the next highest index value 
+
+		//Your class will need the following functions :
+		
+		
+		//	c.Functions for adding 1 or more elements from the middle of the array
+		//	d.Functions for removing from the middle of the array
+		//	i.Both ordered and unordered removal
+		//	e.Functions for changing how much space is allocated for the array
+		//	i.If less space is allocated than is used, the extra used data is discarded
+		//	f.A function for clearing the array
+};
+template<class TYPE>
+DynamicDingus<TYPE>::DynamicDingus()
+{
+	//initialSize 
+	allocatedElements = sizeof(TYPE) * spaceMulti;
+	usedElements = 0;
+	theArray = new TYPE[allocatedElements]();
+//	for (int j = this->getUsedInt(); j < this->getAllocatedInt(); j++)
+//		theArray[j] = TYPE();
+
+
+}
+
+template <class TYPE>
+DynamicDingus<TYPE>::~DynamicDingus()
+{
+
+}
+
+template <class TYPE>
+void DynamicDingus<TYPE>::pushback(TYPE value)
+{
+	//IF array.allocatedElements == array.usedElements THEN
+	if (allocatedElements == usedElements)
+	{
+		//	newData = new array[array.allocatedElements * 2];
+		auto NewDingus = new DynamicDingus<TYPE>[allocatedElements * 2];
+		//copy(newData, array.data, array.allocatedElements);
+		copy(NewDingus, theArray, allocatedElements);
+		//delete array.data
+		delete theArray;
+		//	array.data = newData;
+		theArray = NewDingus;
+		//array.allocatedElements *= 2
+		allocatedElements *= 2;
+	}
+	if(theArray[usedElements] == value)
+	{
+		usedElements += 1 * sizeof(TYPE);
+	}
+	//	END IF
+	//	array.data[array.usedElements] = newElement;
+	//array.usedElements += 1;
+}
+
+template <class TYPE>
+void DynamicDingus<TYPE>::pushfront(TYPE value)
+{
+	//IF array.allocatedElements == array.usedElements THEN
+	if (allocatedElements == usedElements)
+	{
+		//	newData = new array[array.allocatedElements * 2];
+		auto NewDingus = new DynamicDingus<TYPE>[allocatedElements * 2];
+		//copy(newData, array.data, array.allocatedElements);
+		copy(NewDingus, theArray, allocatedElements);
+		//delete array.data
+		delete theArray;
+		//	array.data = newData;
+		theArray = NewDingus;
+		//array.allocatedElements *= 2
+		allocatedElements *= 2;
+	}
+	if (theArray[usedElements] == value)
+	{
+		usedElements += 1 * sizeof(TYPE);
+	}
+	//	END IF
+	//	array.data[array.usedElements] = newElement;
+	//array.usedElements += 1;
+}
+
+template <class TYPE>
+void DynamicDingus<TYPE>::Clear()
+{
+	TYPE* NEWARRAY = new TYPE[sizeof(TYPE) * spaceMulti]();
+	delete this;
+	theArray = NEWARRAY;
+}
+
+template <class TYPE>
+DynamicDingus<TYPE>& DynamicDingus<TYPE>::operator=(const DynamicDingus<TYPE>& other)
+{
+	if (this == &other)
+	{
+		return *this; // I am myself
+	}
+	if (other.isEmpty())
+	{
+		this->Clear(); //the one we are copying from is empty so I need to be too
+	}
+
+	//check if this is smaller if so grow it
+	if (other.getAllocatedInt() >= this->getAllocatedInt())
+	{
+		allocatedElements = other.getAllocatedInt() * 		(sizeof(TYPE) + 10 * sizeof(TYPE));
+	}
+	//copy from 0 - how ever many are allocated to how every are in the other
+	for (int i = 0; i < other.getAllocatedInt(); i++)
+		this[i] = other[i];
+	//fill any remaining spots with new empty TYPES if its larger
+	if (other.getAllocatedInt() < this->getAllocatedInt()) {
+		for (int j = other.getAllocatedInt(); j < this->getAllocatedInt(); j++)
+			this[j] = TYPE();
+	}
+	return this;
+}
+
+template <class TYPE>
+TYPE& DynamicDingus<TYPE>::operator[](int index) {
+
+
+	if (index >= this->getAllocatedInt()) {
+		//number of elements = this far in in TYPE bytes + 10 * TYPE bytes
+		allocatedElements = index * sizeof(TYPE) + 10 * sizeof(TYPE);
+		//the next array cause the old one does not go that high
+		TYPE* NewDingus = new TYPE[allocatedElements];
+		//from 0 - the last index copy in
+		for (int i = 0; i < this->getUsedInt(); i++)
+			NewDingus[i] = theArray[i];
+
+		//initialize the remaining positions using default
+		for (int j = this->getUsedInt(); j < this->getAllocatedInt(); j++)
+			NewDingus[j] = TYPE();
+
+		//delete the old array using the array version of delete
+		delete[] theArray;
+		//the array = next array
+		theArray = NewDingus;
+
+	}
+	//if the position is less than the last index we have accessed
+	if (index > this->getUsedInt())
+		//move forward 1 after where we are
+		usedElements = index*sizeof(TYPE) + sizeof(TYPE);
+	//return the pointer at 
+	return *(theArray + (index * sizeof(TYPE)));
+}
