c6aca3fc3e5c889599311e176b318c0abd54a117
Mondotrasho <oscar@peoplepictures.com.au>
2018-09-24 14:31:10 +1000

    All working acording to test

--- DynamicDingus/main.cpp
+++ DynamicDingus/main.cpp
@@ -1,7 +1,6 @@
 
 #include <cstdlib>
 #include "DynamicDingus.h"
-#include "DynamicArray.h"
 #include <ctime>
 #include <string>
 
@@ -9,56 +8,56 @@ int main()
 {
 	srand(time(NULL));
 //DynamicDingus<int> nums;
-	DynamicArray<int> nums(2,false);
-	for (int i = 0; i < 50; ++i)
+	DynamicDingus<int> nums(2,false);
+	for (auto i = 0; i < 50; ++i)
 	{
-		nums.PushBack(rand() % 200);
+		nums.push_back(rand() % 200);
 	}
 	
 	
 
-	DynamicArray<std::string> box(2,false,false);
-	for (int i = 0; i < 10; ++i)
+	DynamicDingus<std::string> box(2,false,false);
+	for (auto i = 0; i < 10; ++i)
 	{
 		auto a = rand() % 200;
-		auto b = nums.Search(nums, a, 0, nums.GetSize());
-		box.PushBack("search was : " + std::to_string(a));
+		auto b = nums.search(nums, a, 0, nums.get_size());
+		box.push_back("search was : " + std::to_string(a));
 		
 		if (b != -1) {
-			int c = nums[b];
-			box.PushBack("the index its at is : " + std::to_string(b));
+			auto c = nums[b];
+			box.push_back("the index its at is : " + std::to_string(b));
 			if(c == a)
 			{
-				box.PushBack("returned : " + std::to_string(c) + " as Expected");
+				box.push_back("returned : " + std::to_string(c) + " as Expected");
 			}
 			else
 			{
-				box.PushBack("not as expected result was : " + std::to_string(c));
+				box.push_back("not as expected result was : " + std::to_string(c));
 			}
 		}
 		else
 		{
-			box.PushBack("Index not returned -1 returned instead");
-			box.PushBack("Index was -1");
+			box.push_back("Index not returned -1 returned instead");
+			box.push_back("Index was -1");
 		}
 		
 		
 
 	}
-	/*nums[1] = 1;
+	nums[1] = 1;
 	nums[2] = 1;
-	nums[15] = 1;*/
+	nums[15] = 1;
 
-	nums.PushBack(12);
+	nums.push_back(12);
 
-	//nums.Erase(1);
-	//int front = nums.PopFront();
-	//int front1 = nums.PopFront();
-	//int front2 = nums.PopFront();
+	nums.erase(1);
+	auto front = nums.pop_front();
+	auto front1 = nums.pop_front();
+	auto front2 = nums.pop_front();
 
 
-	nums.Insert(-23, 1);
-	nums.PushBack(45);
+	nums.insert(122, 1);
+	nums.push_back(34);
 
 
 	system("pause");
